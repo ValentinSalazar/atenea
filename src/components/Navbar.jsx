1d70fd34722e06a4d1f273ea289b1ca143bca2e3
Facundo Desktop <facundev@outlook.com>
2023-06-26 18:14:57 -0300

    Scalling navbar gap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ export const Navbar = () => {
 
     return(
         <header className="fixed z-10 left-1/2 -translate-x-1/2">
-            <nav className="flex gap-3 justify-center uppercase animate-fade 
-            [&>a]:text-primaryColor text-lg small:text-sm [&>a]:md:text-xl md:gap-10 [&>a]:lg:text-2xl lg:gap-20">
+            <nav className="flex gap-2 justify-center uppercase animate-fade 
+            [&>a]:text-primaryColor text-lg small:text-sm [&>a]:md:text-xl md:gap-5 [&>a]:lg:text-2xl lg:gap-10">
                 <NavLink to="/" className={activeLink}        >Inicio</NavLink>
                 <NavLink to="nosotros" className={activeLink} >Nosotros</NavLink>
                 <NavLink to="servicios" className={activeLink}>Servicios</NavLink>
